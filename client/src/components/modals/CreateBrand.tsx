dc1f3ff0b282bf0eb04f53883bf80ef93cbbb5c4
vitaly.khe <vitaly.khe@gmail.com>
2021-10-24 20:06:31 +0400

    admin functions

--- client/src/components/modals/CreateBrand.tsx
+++ client/src/components/modals/CreateBrand.tsx
@@ -1,16 +1,40 @@
 
 import React, { FC } from 'react'
-import { Modal } from 'react-bootstrap/Modal'
-import { Stack } from 
-interface IProps {}
+import { Button, Form } from 'react-bootstrap'
+import Modal from 'react-bootstrap/Modal'
+
+interface IProps {
+  show: boolean;
+  onHide: () => void;
+}
 
 /**
 * @author
 * @function @CreateBrand
 **/
 
-export const CreateBrand:FC<IProps> = (props) => {
+export const CreateBrand:FC<IProps> = ({show, onHide}) => {
   return (
-    <div>CreateBrand</div>
+    <Modal
+      show={show}
+      onHide={onHide}
+      size="lg"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Add new band
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Control placeholder={'Enter new brand name'}/>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>  
+        <Button variant={'outline'} onClick={()=> {}}>Add</Button>
+        <Button variant={'outline'} onClick={onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
    )
  }
--- client/src/components/modals/CreateType.tsx
+++ client/src/components/modals/CreateType.tsx
@@ -1,7 +1,11 @@
 import React, { FC } from 'react'
+import { Button, Form } from 'react-bootstrap'
+import Modal from 'react-bootstrap/Modal'
 
-interface IProps {}
-
+interface IProps {
+  show: boolean;
+  onHide: () => void;
+}
 /**
 * @author
 * @function @CreateType
@@ -9,6 +13,25 @@ interface IProps {}
 
 export const CreateType:FC<IProps> = (props) => {
   return (
-    <div>CreateType</div>
-   )
+    <Modal
+      show={show}
+      onHide={onHide}
+      size="lg"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Add new band
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Control placeholder={'Enter new type name'}/>
+        </Form>
+      </Modal.Body>
+      <Modal.Footer>  
+        <Button variant={'outline'} onClick={()=> {}}>Add</Button>
+        <Button variant={'outline'} onClick={onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>   )
  }
