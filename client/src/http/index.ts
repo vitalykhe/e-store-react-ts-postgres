15b4c929363425a8d977acdac5f912f98d9e6dbe
vitaly.khe <vitaly.khe@gmail.com>
2021-10-25 18:43:02 +0400

    axios

--- client/src/http/index.ts
+++ client/src/http/index.ts
@@ -1,6 +1,27 @@
-import axios, { AxiosInstance } from "axios";
- 
+import axios, { AxiosInstance, AxiosRequestConfig } from "axios";
 
-const $host = axios.create({
-    baseURL: process.env.SERVER_API_URL;
+//to api that don't require auth
+const $noAuthHost = axios.create({
+    baseURL: process.env.SERVER_API_URL
 })
+
+//to api that require auth
+const $authHost = axios.create({
+    baseURL: process.env.SERVER_API_URL 
+})
+
+
+//eslint
+const authInterceptor = (config: AxiosRequestConfig) => {
+    if (config && config.headers)
+        config.headers.authorization = `Bearer ${localStorage.getItem('token')}`
+    return config
+}
+
+
+$authHost.interceptors.request.use(authInterceptor)
+
+ export {
+     $noAuthHost,
+     $authHost
+ }
---
+++ client/src/http/userApi.ts
@@ -0,0 +1,2 @@
+import { $authHost, $noAuthHost } from './index'
+
