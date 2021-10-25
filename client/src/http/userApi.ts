4c30afb4cb6c859771168e165ebb66b4b88b519e
vitaly.khe <vitaly.khe@gmail.com>
2021-10-25 18:59:06 +0400

    ax2

--- client/src/http/userApi.ts
+++ client/src/http/userApi.ts
@@ -1,2 +1,5 @@
 import { $authHost, $noAuthHost } from './index'
 
+export const registration = async (email: string, password: string) => {
+    const response = await $noAuthHost.post('api/auth/registration')
+}
